d2f9513a946328e79f07e027507e15de5fe5fd4b
wyatt-avilla <wyattmurphy1@gmail.com>
2024-06-14 14:14:17 -0700

    pre commit

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,16 @@ async fn main() {
     dotenv::dotenv().ok();
 
     println!("Hello, world!");
+
+    match get_recently_listened(5).await {
+        Ok(songvec) => {
+            for song in songvec {
+                println!(
+                    "{} by {} on {}",
+                    song.title, song.artist_name, song.album_name
+                );
+            }
+        }
+        Err(e) => eprintln!("function failed with: {}", e),
+    }
 }

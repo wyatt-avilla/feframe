58ab6a98f7e7236134a3649933e667d726f78c42
wyatt-avilla <wyattmurphy1@gmail.com>
2024-06-18 09:55:55 -0700

    unused code bandaid function

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,23 @@
-use dynamic_content::{Book, Commit, Movie, Song};
+use dynamic_content::{ApiRefresh, Book, Commit, Movie, Song};
 
 mod dynamic_content;
+
+#[allow(dead_code)]
+async fn use_content() {
+    let commit = &Commit::fetch_newest(1).await.unwrap()[0];
+    let book = &Book::fetch_newest(1).await.unwrap()[0];
+    let song = &Song::fetch_newest(1).await.unwrap()[0];
+    let movie = &Movie::fetch_newest(1).await.unwrap()[0];
+
+    println!(
+        "{}, {}, {}, {}",
+        commit.repository_name, book.title, song.title, movie.title
+    );
+}
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
-
-    let url = "https://github.com/wyatt-avilla/pulse";
-
-    let c = Commit {
-        repository_name: String::new(),
-        message: String::new(),
-        url: url::Url::parse("").unwrap(),
-        repository_link: url::Url::parse(url).unwrap(),
-    };
-
-    let b = Book {
-        author: String::new(),
-        title: String::new(),
-        author_url: url::Url::parse(url).unwrap(),
-        title_url: url::Url::parse(url).unwrap(),
-    };
-
-    let s = Song {
-        title: String::new(),
-        artist_name: String::new(),
-        album_name: String::new(),
-        album_image: url::Url::parse(url).unwrap(),
-        url: url::Url::parse(url).unwrap(),
-    };
-
-    let m = Movie {
-        title: String::new(),
-        rating: String::new(),
-        url: url::Url::parse(url).unwrap(),
-    };
-
-    println!(
-        "{}, {}, {}, {}",
-        c.repository_name, b.title, s.title, m.title
-    );
 
     println!("Hello, world!");
 }

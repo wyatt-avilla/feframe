7f5d8c0ff7bd30aa139c8f2a8661f5e8091b4303
wyatt-avilla <wyattmurphy1@gmail.com>
2024-06-20 17:48:52 -0700

    svg element alignment

--- frontend/src/components/lastfm/row.rs
+++ frontend/src/components/lastfm/row.rs
@@ -14,7 +14,7 @@ pub fn Row(props: &Song) -> Html {
     } = props;
 
     let play_svg = html! {
-            <svg width="16px" height="16px" viewBox="-1 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <svg width="16px" height="16px" viewBox="-1 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                 <g id="Dribbble-Light-Preview" transform="translate(-65.000000, -3803.000000)" fill="#000000">
                     <g id="icons" transform="translate(56.000000, 160.000000)">

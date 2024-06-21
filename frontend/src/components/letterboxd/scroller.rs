b8b66244b102767156b97e3e539bf2ad08aab9cf
wyatt-avilla <wyattmurphy1@gmail.com>
2024-06-21 09:46:27 -0700

    naming in letterboxd component

--- frontend/src/components/letterboxd/scroller.rs
+++ frontend/src/components/letterboxd/scroller.rs
@@ -7,18 +7,18 @@ use yew::prelude::*;
 #[styled_component]
 pub fn Scroller() -> Html {
     #[allow(clippy::redundant_closure)]
-    let songs = use_state(|| std::vec::Vec::new());
+    let movies = use_state(|| std::vec::Vec::new());
     {
-        let songs = songs.clone();
+        let movies = movies.clone();
         use_effect_with((), move |()| {
-            let songs = songs.clone();
+            let movies = movies.clone();
             wasm_bindgen_futures::spawn_local(async move {
                 let response =
                     reqwest::get(format!("http://{}{}", ENDPOINT.base, ENDPOINT.letterboxd))
                         .await
                         .unwrap();
-                let fetched_songs: Vec<Movie> = response.json().await.unwrap();
-                songs.set(fetched_songs);
+                let fetched_movies: Vec<Movie> = response.json().await.unwrap();
+                movies.set(fetched_movies);
             });
             || ()
         });
@@ -54,7 +54,7 @@ pub fn Scroller() -> Html {
             scrollbar-width: none;
             -ms-overflow-style: none; /* Internet Explorer 10+ */
         "#)}>
-            { songs.iter().map(|commit| html! { <Row ..commit.clone() /> }).collect::<Vec<_>>() }
+            { movies.iter().map(|movie| html! { <Row ..movie.clone() /> }).collect::<Vec<_>>() }
         </div>
     }
 }

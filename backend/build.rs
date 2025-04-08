2cb3c911e23c7fc78f043b41a2cfa4c527e416fc
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-04-08 00:40:25 -0700

    dont use backend's `build.rs`

--- backend/build.rs
+++
@@ -1,9 +0,0 @@
-use std::fs;
-use std::path::Path;
-
-fn main() {
-    let dist_dir = Path::new("../frontend/dist");
-    if !dist_dir.exists() || fs::read_dir(dist_dir).map_or(true, |entries| entries.count() == 0) {
-        println!("cargo:warning=Unable to serve frontened");
-    }
-}

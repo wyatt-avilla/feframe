c259c5cd6d85162d2352b3b69c17b2fb0586ffc8
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-09-08 15:57:25 -0700

    fix letterboxd scraping

--- backend/src/fetching/letterboxd.rs
+++ backend/src/fetching/letterboxd.rs
@@ -1,39 +1,6 @@
 use cached::proc_macro::once;
-use regex::Regex;
 use scraper::{Html, Selector};
 use types::Movie;
-
-fn parse_image(html: &Html) -> Result<String, Box<dyn std::error::Error>> {
-    let img_selector =
-        Selector::parse("div.react-component.poster.film-poster img.image[src]").unwrap();
-
-    Ok(html
-        .select(&img_selector)
-        .next()
-        .ok_or("Image source not found in HTML")?
-        .attr("src")
-        .ok_or("Image source attribute not found in HTML")?
-        .to_string())
-}
-
-fn parse_release_year(html: &Html) -> Result<String, Box<dyn std::error::Error>> {
-    let frame_selector = Selector::parse("span.frame[title]").unwrap();
-
-    let date_re = Regex::new(r"\((\d{4})\)").unwrap();
-
-    let title = html
-        .select(&frame_selector)
-        .next()
-        .ok_or("Frame not found in HTML")?
-        .attr("title")
-        .ok_or("Title not found in frame HTML")?;
-
-    Ok(date_re
-        .captures(title)
-        .and_then(|caps| caps.get(1))
-        .map(|year| year.as_str().to_owned())
-        .ok_or("Couldn't parse date")?)
-}
 
 // 1 day
 #[once(result = true, time = 86400, sync_writes = true)]
@@ -52,40 +19,49 @@ pub async fn fetch_newest(
             .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)?,
     );
 
-    let row_selector = Selector::parse("li.poster-container").unwrap();
+    let container_sel =
+        Selector::parse("#content .cols-2.overflow section.section.col-main.overflow").unwrap();
+    let poster_grid_sel = Selector::parse("div.poster-grid").unwrap();
+    let griditem_sel = Selector::parse("li.griditem").unwrap();
 
-    let div_selector = Selector::parse("div.really-lazy-load").unwrap();
-    let rating_selector = Selector::parse("span.rating").unwrap();
-    let img_selector = Selector::parse("img.image").unwrap();
+    let react_sel = Selector::parse("div.react-component").unwrap();
+    let img_sel = Selector::parse("img.image").unwrap();
+    let rating_sel = Selector::parse("p.poster-viewingdata span.rating").unwrap();
 
-    let movie_iter = html
-        .select(&row_selector)
-        .filter_map(|row| {
-            let title = row
-                .select(&img_selector)
-                .next()?
-                .value()
-                .attr("alt")?
-                .to_string();
+    let base = html.select(&container_sel).next();
 
-            let rating = row
-                .select(&rating_selector)
+    let movie_iter = base
+        .into_iter()
+        .flat_map(move |container| {
+            let griditem_sel = &griditem_sel.clone();
+            container
+                .select(&poster_grid_sel.clone())
+                .flat_map(|pg| pg.select(griditem_sel))
+                .collect::<Vec<_>>()
+        })
+        .filter_map(move |li| {
+            let title = li.select(&img_sel).next()?.value().attr("alt")?.to_string();
+
+            let rating = li
+                .select(&rating_sel)
                 .next()
-                .map(|r| r.inner_html())
-                .filter(|r| !r.is_empty())?;
+                .map(|r| r.text().collect::<String>().trim().to_string())
+                .filter(|s| !s.is_empty())?;
 
-            let div_val = row.select(&div_selector).next()?.value();
+            let rc = li.select(&react_sel).next()?;
+            let attrs = rc.value();
 
-            let link = div_val.attr("data-target-link")?;
-
-            let slug = div_val.attr("data-film-slug")?;
+            let link = attrs.attr("data-target-link")?;
+            let slug = attrs
+                .attr("data-item-slug")
+                .or_else(|| attrs.attr("data-film-slug"))?;
 
             Some(Movie {
                 title,
                 rating,
                 release_year: String::new(),
-                url: format! {"https://letterboxd.com{link}"},
-                poster_url: slug.to_string(), // icky, just store the slug in here for now xd
+                url: format!("https://letterboxd.com{link}"),
+                poster_url: slug.to_string(),
             })
         })
         .take(n as usize);
@@ -93,20 +69,35 @@ pub async fn fetch_newest(
     // no async closures :(
     let mut movies = Vec::new();
     for mut movie in movie_iter {
-        let html = Html::parse_document(
-            &reqwest::get(format!(
-                "https://letterboxd.com/ajax/poster/film/{}/std/70x105/",
-                movie.poster_url // aka slug
-            ))
+        let slug = movie.poster_url;
+
+        let poster_json = &reqwest::get(format!(
+            "https://letterboxd.com/film/{slug}/poster/std/150/"
+        ))
+        .await
+        .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)?
+        .json::<serde_json::Value>()
+        .await
+        .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)?;
+
+        let movie_json = &reqwest::get(format!("https://letterboxd.com/film/{slug}/json/"))
             .await
             .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)?
-            .text()
+            .json::<serde_json::Value>()
             .await
-            .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)?,
-        );
+            .map_err(|err| Box::new(err) as Box<dyn std::error::Error>)?;
 
-        movie.poster_url = parse_image(&html)?;
-        movie.release_year = parse_release_year(&html)?;
+        movie.poster_url = poster_json
+            .get("url")
+            .ok_or("Missing poster url")?
+            .to_string()
+            .trim_matches('"')
+            .to_string();
+
+        movie.release_year = movie_json
+            .get("releaseYear")
+            .ok_or("Missing release year")?
+            .to_string();
 
         movies.push(movie);
     }
